1c0367b97b08156576c676a118974d22e9a31ff7
mattia <mattialopardo@gmail.com>
2026-09-23 17:00:12 +0200

    Ajout du fichier CMakeLists.txt, afin de pouvoir run main.cpp

--- main.cpp
+++ main.cpp
@@ -5,3 +5,12 @@ Date :
 But : Bureau de change 
 Remarque(s) : 
 --------------------------- */
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << endl;
+}
