a1b66a976bcc3596ca2bc31b395d1b58ccb0fd06
mattia <mattialopardo@gmail.com>
2026-09-23 18:16:46 +0200

    code fonctionnelle, reste à faire le ticket de sortie

--- main.cpp
+++ main.cpp
@@ -1,16 +1,45 @@
 /* --------------------------- 
 Laboratoire : 03
-Auteur(s) : 
-Date : 
+Auteur(s) : Mattia Lopardo
+Date : 23.09.26
 But : Bureau de change 
 Remarque(s) : 
 --------------------------- */
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <windows.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    int numcompte = 0;
+    char nom;
+    double euro = 0.0; //Somme en Euro que l'on veut retirer
+    double soldecompte = 1000.0;
+    double tauxchange = 1.024;
+    double fraix = 5.0;
+
+    SetConsoleOutputCP(CP_UTF8); // Permet d'afficher des lettres accentuées
+
+    cout << "Quel est votre numéro de compte ?" << endl;
+    cin >> numcompte;
+    cout << "Quel est votre nom de famille ?" << endl;
+    cin >> nom;
+
+    cout << "Solde de votre compte CHF : " << soldecompte << endl;
+    cout << "Taux de change : 1 CHF = " << tauxchange << " Euro" << endl;
+    cout << "Frais d'opération : " << fraix << " CHF" << endl;
+
+    cout << "Entrez la somme souhaitée en Euro :" << endl;
+    cin >> euro;
+
+    double francs = euro/tauxchange; //Fait la conversion d'euro à francs
+    double restcompte = soldecompte - francs; //Soustrait la somme retirée au solde du compte
+    francs = round(francs * 100.0) / 100.0; //Permet d'arrondir à 2 décimale après la virgule
+    restcompte = round(restcompte * 100.0) / 100.0; // idem
+
+    cout << "Somme CHF : " << francs << ", Solde compte : " << restcompte << endl;
+
+    return EXIT_SUCCESS;
 }
